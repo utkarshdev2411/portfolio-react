5b8843955da844179fe6a4fcc0e39cfb67b1861b
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-12-30 18:30:48 +0530

    Refactor App component structure and styles; implement smooth scrolling for sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Components/Navbar/navbar";
-import Intro from "./Components/Intro/intro"
-import Services from "./Components/Services/services"
+import Intro from "./Components/Intro/intro";
+import Services from "./Components/Services/services";
 import Experience from "./Components/Experience/experience";
 import Work from "./Components/Work/work";
 import Portfolio from "./Components/Portfolio/portfolio";
@@ -9,28 +9,35 @@ import Contact from "./Components/Contact/contact";
 import Footer from "./Components/Footer/footer";
 import { themeContext } from "./Context";
 import { useContext } from "react";
-// import Skills from "./Components/Skills/skills";
+import { Element } from "react-scroll";
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
 
-
-      const theme=useContext(themeContext)
-      const darkMode= theme.state.darkMode;
   return (
-    <div className="App" style={{background: darkMode?'black':'',color: darkMode?'white':''}}>
-     
-     <Navbar/>
-     <Intro/>
-     <Services/>
-     <Experience/>
-     {/* <Work/>
-     <Portfolio/>
-     
-
-     <Contact/>
-     <Footer/> */}
+    <div className="App" style={{ background: darkMode ? 'black' : '', color: darkMode ? 'white' : '' }}>
+      <Navbar />
+      <Element name="intro">
+        <Intro />
+      </Element>
+      <Element name="services">
+        <Services />
+      </Element>
+      <Element name="experience">
+        <Experience />
+      </Element>
+      <Element name="work">
+        <Work />
+      </Element>
+      {/* <Element name="portfolio">
+        <Portfolio />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element> */}
+      <Footer />
     </div>
-   
   );
 }
 
